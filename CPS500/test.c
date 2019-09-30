15795fbd9ed077812963730800459149b5d5b260
kebin <kemartin42@gmail.com>
2019-09-29 19:29:23 -0700

    9/29/19

---
+++ CPS500/test.c
@@ -0,0 +1,12 @@
+/*=================================================*\
+**
+**  File Name     : test.c
+**  Creation Date : 09-29-2019
+**  Last Modified : Sun 29 Sep 2019 07:28:57 PM PDT
+**  Compiler      : clang
+**  Author        : Kevin Martin, kmarti44@syr.edu
+**  Organization  : Syracuse University
+***=================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
